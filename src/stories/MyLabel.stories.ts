8770fcb9393ac3403ed0bff7d5a1550e0c3177b9
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2024-07-15 23:19:40 -0500

    Updates

--- src/stories/MyLabel.stories.ts
+++ src/stories/MyLabel.stories.ts
@@ -1,16 +1,16 @@
-import {Meta, StoryObj} from '@storybook/react';
-import {MyLabel} from '../components/MyLabel';
+import { Meta, StoryObj } from "@storybook/react";
+import { MyLabel } from "../components/MyLabel";
 
 const meta: Meta<typeof MyLabel> = {
-    title: 'Example/MyLabel',
-    component: MyLabel,
-    tags: ['autodocs'],
-    parameters: {
-        layout: 'centered',
-    },
-    argTypes: {
-        color: { control: 'inline-radio'},
-    },
+  title: "Example/MyLabel",
+  component: MyLabel,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    color: { control: "inline-radio" },
+  },
 };
 
 export default meta;
@@ -18,40 +18,39 @@ export default meta;
 type Story = StoryObj<typeof MyLabel>;
 
 export const Basic: Story = {
-    args: {
-        label: 'Hello World',
-    },
+  args: {
+    label: "Hello World",
+  },
 };
 
 export const AllCaps: Story = {
-    args: {
-        label: 'All Caps label',
-        size: 'h1',
-        allCaps: true,
-        color: 'text-tertiary',
-        fontColor: 'red',
-    },
+  args: {
+    label: "All Caps label",
+    size: "h1",
+    allCaps: true,
+    color: "text-tertiary",
+    fontColor: "red",
+  },
 };
 
 export const Secondary: Story = {
-    args: {
-        label: 'Secondary label',
-        size: 'h1',
-    },
+  args: {
+    label: "Secondary label",
+    size: "h1",
+  },
 };
 
-
 export const CustomColor: Story = {
-    args: {
-        label: 'Custom Color label',
-        size: 'h1',
-    },
+  args: {
+    label: "Custom Color label",
+    size: "h1",
+  },
 };
 
 export const CustomFont: Story = {
-    args: {
-        label: 'Custom Font label',
-        size: 'h1',
-        allCaps: true,
-    },
+  args: {
+    label: "Custom Font label",
+    size: "h1",
+    allCaps: true,
+  },
 };
